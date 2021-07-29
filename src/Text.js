5a5b735637f29ca6f47ec88b12430bf9ccf925e4
okoroname <okoroname@rocketmail.com>
2021-07-29 10:56:36 -0700

    first commit

---
+++ src/Text.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+
+export default class Text extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+import React from 'react'
+
+export default function Text() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+import React from 'react'
+
+function Text() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+
